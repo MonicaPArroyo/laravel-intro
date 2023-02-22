408075f2232930edb652bcdfc263284311d8e98d
Mónica P. Arroyo <monicaparroyo7@gmail.com>
2023-02-22 10:59:46 -0600

    Clase 4: Routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+/**
+ * Route::get       | Consultar
+ * Route::post      | Guardar
+ * Route::delete    | Eliminar
+ * Route::put       | Actualizar
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Ruta home';
+});
+
+Route::get('blog', function () {
+    return 'Listado de publicaciones';
+});
+
+Route::get('blog/{slug}', function ($slug) {
+    // Simular consulta a base de datos
+    return $slug;
+});
+
+Route::get('buscar', function (Request $request) {
+    // Simular consulta a base de datos
+    return $request->all();
 });
