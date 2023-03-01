126b0a6da73dd58a6b4b4ea0288378127687257b
Mónica P. Arroyo <monicaparroyo7@gmail.com>
2023-03-01 12:57:36 -0600

    Clase 23 - Registros duplicados

--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -1,11 +1,14 @@
 @csrf
 
 <label class="uppercase text-gray-700 dark:text-gray-100 text-xs">Título</label>
-<span class="text-xs text-red-600">@error('title') {{ $message}} @enderror</span>
+<span class="text-xs text-red-600">@error('title') {{ $message }} @enderror</span>
 <input type="text" name="title" class="rounded dark:text-gray-800 border-gray-600 w-full mb-4" value="{{ old('title', $post->title) }}">
 
+<label class="uppercase text-gray-700 dark:text-gray-100 text-xs">Slug</label>
+<span class="text-xs text-red-600">@error('slug') {{ $message }} @enderror</span>
+<input type="text" name="slug" class="rounded dark:text-gray-800 border-gray-600 w-full mb-4" value="{{ old('slug', $post->slug) }}">
 <label class="uppercase text-gray-700 dark:text-gray-100 text-xs">Contenido</label>
-<span class="text-xs text-red-600">@error('body') {{ $message}} @enderror</span>
+<span class="text-xs text-red-600">@error('body') {{ $message }} @enderror</span>
 <textarea name="body" rows="5" class="rounded dark:text-gray-800 border-gray-600 w-full mb-4">{{ old('body', $post->body) }}</textarea>
 
 <div class="flex justify-between items-center">
