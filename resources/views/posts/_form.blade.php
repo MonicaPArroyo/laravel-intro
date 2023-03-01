6b4e3cc924bbaec1dd10686278b23ac168124432
Mónica P. Arroyo <monicaparroyo7@gmail.com>
2023-03-01 12:45:58 -0600

    Clase 22 - Validacion

--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -1,10 +1,12 @@
 @csrf
 
 <label class="uppercase text-gray-700 dark:text-gray-100 text-xs">Título</label>
-<input type="text" name="title" class="rounded dark:text-gray-800 border-gray-600 w-full mb-4" value="{{ $post->title }}">
+<span class="text-xs text-red-600">@error('title') {{ $message}} @enderror</span>
+<input type="text" name="title" class="rounded dark:text-gray-800 border-gray-600 w-full mb-4" value="{{ old('title', $post->title) }}">
 
 <label class="uppercase text-gray-700 dark:text-gray-100 text-xs">Contenido</label>
-<textarea name="body" rows="5" class="rounded dark:text-gray-800 border-gray-600 w-full mb-4">{{ $post->body }}</textarea>
+<span class="text-xs text-red-600">@error('body') {{ $message}} @enderror</span>
+<textarea name="body" rows="5" class="rounded dark:text-gray-800 border-gray-600 w-full mb-4">{{ old('body', $post->body) }}</textarea>
 
 <div class="flex justify-between items-center">
     <a href="{{ route('posts.index') }}" class="text-indigo-600">Volver</a>
